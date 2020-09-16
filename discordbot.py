f21a7a314cd8361170ff825a95e0a5e59a9c2a69
sfuj1wara <sfuj1.201@gmail.com>
2020-09-16 23:42:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,26 +1,28 @@
-from discord.ext import commands
+# インストールした discord.py を読み込む
+import discord
 import os
-import traceback
-import requests
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 line_token = os.environ['LINE_BOT_TOKEN']
 
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('hello')
-    payload = {'message':'hello'} # 通知させたいメッセージ
-    headers = {'Authorization':'Bearer ' + line_token} # 発行したトークン
-    r = requests.post('https://notify-api.line.me/api/notify', data=payload, headers=headers)
-
-
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
