5548e0adbffd3883ba1e044de2965be0366c55c7
sfuj1wara <sfuj1.201@gmail.com>
2020-09-16 23:24:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,11 @@
 from discord.ext import commands
 import os
 import traceback
+import requests
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+line_token = os.environ['LINE_BOT_TOKEN']
 
 
 @bot.event
@@ -16,6 +18,9 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('hello')
+    payload = {'message':'test'} # 通知させたいメッセージ
+    headers = {'Authorization':'Bearer ' + line_token} # 発行したトークン
+    r = requests.post('https://notify-api.line.me/api/notify', data=payload, headers=headers)
 
 
 bot.run(token)
